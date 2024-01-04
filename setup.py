22fce9d8c32062615754253b04d91059a8489e0b
Dave Deriso <deriso@gmail.com>
2024-01-04 13:44:03 -0800

    new version

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ with open("readme.md","r") as f:
 
 setup_params = setup(
   name='gdtw',
-  version='1.1.1',
+  version='1.1.2',
   author='Dave Deriso',
   author_email='dderiso@alumni.stanford.edu',
   description='General Dynamic Time Warping',
